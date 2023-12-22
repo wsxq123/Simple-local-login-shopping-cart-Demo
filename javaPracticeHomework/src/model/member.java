1fba5ba3bd3e989424842963eef6cd26a3a984ca
wsxq123 <51814501+wsxq123@users.noreply.github.com>
2023-12-22 16:23:11 +0800

    model.member /model.order

--- javaPracticeHomework/src/model/member.java
+++ javaPracticeHomework/src/model/member.java
@@ -1,5 +1,68 @@
 package model;
 
 public class member {
+	private Integer id;
+	private String  name;
+	private Integer birthdate;
+	private Integer gender;
+	private Integer phone;
+	private String  address;
+
+	public member(String name, Integer birthdate, Integer gender, Integer phone, String address) {
+		super();
+		this.name = name;
+		this.birthdate = birthdate;
+		this.gender = gender;
+		this.phone = phone;
+		this.address = address;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Integer getBirthdate() {
+		return birthdate;
+	}
+
+	public void setBirthdate(Integer birthdate) {
+		this.birthdate = birthdate;
+	}
+
+	public Integer getGender() {
+		return gender;
+	}
+
+	public void setGender(Integer gender) {
+		this.gender = gender;
+	}
+
+	public Integer getPhone() {
+		return phone;
+	}
+
+	public void setPhone(Integer phone) {
+		this.phone = phone;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
 
 }
--- javaPracticeHomework/src/model/order.java
+++ javaPracticeHomework/src/model/order.java
@@ -1,5 +1,40 @@
 package model;
 
 public class order {
+	private Integer sellerID;
+	private String  password;
+	private Integer name;
+	
+	public order(Integer sellerID, String password, Integer name) {
+		super();
+		this.sellerID = sellerID;
+		this.password = password;
+		this.name = name;
+	}
 
+	public Integer getSellerID() {
+		return sellerID;
+	}
+
+	public void setSellerID(Integer sellerID) {
+		this.sellerID = sellerID;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Integer getName() {
+		return name;
+	}
+
+	public void setName(Integer name) {
+		this.name = name;
+	}
+	
+	
 }
