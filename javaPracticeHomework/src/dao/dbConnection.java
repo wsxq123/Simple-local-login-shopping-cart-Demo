fabf14515c07e6e1630f021ebb66c6c09c813237
wsxq123 <51814501+wsxq123@users.noreply.github.com>
2023-12-25 16:26:09 +0800

    db connect & memberDao

--- javaPracticeHomework/src/dao/dbConnection.java
+++ javaPracticeHomework/src/dao/dbConnection.java
@@ -1,10 +1,33 @@
 package dao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class dbConnection {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println(dbConnection.getDB());
 	}
 
+	private static Connection getDB() {
+		Connection conn = null;
+		String url = "jdbc:mysql://localhost:3306/company";
+		String user = "root";
+		String password = "12345";
+
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			conn = DriverManager.getConnection(url, user, password);
+
+		} catch (ClassNotFoundException e) {
+
+			e.printStackTrace();
+
+		} catch (SQLException e) {
+
+			e.printStackTrace();
+		}
+		return conn;
+	}
 }
--- javaPracticeHomework/src/dao/memberDao.java
+++ javaPracticeHomework/src/dao/memberDao.java
@@ -1,5 +1,31 @@
 package dao;
 
+import java.util.List;
+
+import model.member;
+
 public interface memberDao {
+//create
+	void createMember(member m);
+	
+//read
+	//single member search (memberSearchUI)
+	member queryMember(int phone,int birthday);
+	
+	//all member search (memberSearchUI)
+	List<member> queryAllMember();
+	
+	//check member exist (memberLoginUI)
+	boolean queryMemberAccount(int phone,int birthday);
+	
+//update 
+	//(memberSearchUI)
+	void updateMember(member m);
+	
+	
+//delete 
+	//(memberSearchUI)
+	void deleteMember(int member_id);
+	
 
 }
