dcdbfe9d7badb8ed712fda091aee251e00d26fa1
wsxq123 <51814501+wsxq123@users.noreply.github.com>
2023-12-28 15:48:41 +0800

    some orderAddUI

--- javaPracticeHomework/src/controller/order/orderAddUI.java
+++ javaPracticeHomework/src/controller/order/orderAddUI.java
@@ -30,12 +30,19 @@ import javax.swing.JTextPane;
 import javax.swing.ListSelectionModel;
 import java.awt.Component;
 import java.awt.Dimension;
+import javax.swing.JTextArea;
+import java.awt.FlowLayout;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+import java.awt.BorderLayout;
+import javax.swing.BoxLayout;
 
 public class orderAddUI extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
 	private JTable table;
+	private JTextField textField;
 
 	/**
 	 * Launch the application.
@@ -75,7 +82,7 @@ public class orderAddUI extends JFrame {
 		
 		
 		JPanel panel = new JPanel();
-		panel.setBounds(10, 81, 554, 252);
+		panel.setBounds(10, 81, 554, 169);
 		contentPane.add(panel);
 		panel.setLayout(null);
 		
@@ -87,27 +94,34 @@ public class orderAddUI extends JFrame {
 		table.setShowHorizontalLines(false);
 		table.setModel(new DefaultTableModel(
 			new Object[][] {
-				{"a", "s", "d"},
-				{"d", "d", "d"},
-				{"d", "d", null},
+				{"bodyShape_coolSculpting", "12999"},
+				{"bodyShape_starformer", "15999"},
+				{"facialLaser_ECO2 Laser", "8999"},
+				{"facialLaser_picoWay", "4999"},
 			},
 			new String[] {
-				"name", "price", "New column"
+				"name", "price"
 			}
 		) {
 			Class[] columnTypes = new Class[] {
-				String.class, String.class, Object.class
+				String.class, String.class
 			};
 			public Class getColumnClass(int columnIndex) {
 				return columnTypes[columnIndex];
 			}
 		});
-		table.getColumnModel().getColumn(0).setMinWidth(50);
+		table.getColumnModel().getColumn(0).setPreferredWidth(150);
+		table.getColumnModel().getColumn(0).setMinWidth(150);
 		table.getColumnModel().getColumn(1).setPreferredWidth(90);
+		table.getColumnModel().getColumn(1).setMinWidth(5);
 		table.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
 		table.setFont(new Font("新細明體", Font.PLAIN, 18));
-		table.setBounds(10, 66, 300, 128);
+		table.setBounds(10, 10, 313, 220);
 		panel.add(table);
+		
+		JPanel panel_2 = new JPanel();
+		panel_2.setBounds(333, 10, 157, 130);
+		panel.add(panel_2);
 		
 		JButton btnNewButton = new JButton("");
 		btnNewButton.addActionListener(new ActionListener() {
@@ -144,8 +158,58 @@ public class orderAddUI extends JFrame {
 		
 		comboBox.setSelectedItem("select service");
 		
-		JButton btnNewButton_1 = new JButton("New button");
-		btnNewButton_1.setBounds(477, 343, 87, 23);
+		JButton btnNewButton_1 = new JButton("");
+		btnNewButton_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnNewButton_1.setContentAreaFilled(false);
+		btnNewButton_1.setBorderPainted(false);
+		btnNewButton_1.setBackground(Color.WHITE);
+		btnNewButton_1.setBounds(487, 334, 77, 32);
 		contentPane.add(btnNewButton_1);
+		btnNewButton_1.setIcon(new ImageIcon("C:\\javaOCP\\javaPracticeShop\\javaPracticeHomework\\asset\\AddBtn.png"));
+		
+		JButton btnNewButton_2_1 = new JButton("");
+		btnNewButton_2_1.setIcon(new ImageIcon("C:\\javaOCP\\javaPracticeShop\\javaPracticeHomework\\asset\\minusBtn.png"));
+		btnNewButton_2_1.setBounds(403, 21, 21, 23);
+		contentPane.add(btnNewButton_2_1);
+		btnNewButton_2_1.setBorderPainted(false);
+		
+		JButton btnNewButton_2 = new JButton("");
+		btnNewButton_2.setBounds(343, 21, 21, 23);
+		contentPane.add(btnNewButton_2);
+		btnNewButton_2.setBorderPainted(false);
+		btnNewButton_2.setIcon(new ImageIcon("C:\\javaOCP\\javaPracticeShop\\javaPracticeHomework\\asset\\plusBtn.png"));
+		
+		JTextArea textArea = new JTextArea();
+		textArea.setText("0");
+		textArea.setBounds(363, 19, 30, 25);
+		contentPane.add(textArea);
+		
+		JPanel panel_1 = new JPanel();
+		panel_1.setBounds(43, 272, 134, 25);
+		contentPane.add(panel_1);
+		panel_1.setLayout(new BoxLayout(panel_1, BoxLayout.X_AXIS));
+		
+		JButton btnNewButton_2_2 = new JButton("");
+		btnNewButton_2_2.setContentAreaFilled(false);
+		btnNewButton_2_2.setIcon(new ImageIcon("C:\\javaOCP\\javaPracticeShop\\javaPracticeHomework\\asset\\plusBtn.png"));
+		btnNewButton_2_2.setBorderPainted(false);
+		panel_1.add(btnNewButton_2_2);
+		
+		textField = new JTextField();
+		textField.setHorizontalAlignment(SwingConstants.CENTER);
+		textField.setFont(new Font("新細明體", Font.PLAIN, 14));
+		textField.setPreferredSize(new Dimension(4, 21));
+		textField.setText("0");
+		panel_1.add(textField);
+		textField.setColumns(10);
+		
+		JButton btnNewButton_2_1_1 = new JButton("");
+		btnNewButton_2_1_1.setContentAreaFilled(false);
+		btnNewButton_2_1_1.setIcon(new ImageIcon("C:\\javaOCP\\javaPracticeShop\\javaPracticeHomework\\asset\\minusBtn.png"));
+		btnNewButton_2_1_1.setBorderPainted(false);
+		panel_1.add(btnNewButton_2_1_1);
 	}
 }
