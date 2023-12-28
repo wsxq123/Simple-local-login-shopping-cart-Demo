598d0def95cc79b6ef29bbcad807d7ae966a1c30
wsxq123 <51814501+wsxq123@users.noreply.github.com>
2023-12-28 12:04:20 +0800

    orderAddUI

--- javaPracticeHomework/src/controller/order/orderAddUI.java
+++ javaPracticeHomework/src/controller/order/orderAddUI.java
@@ -15,14 +15,27 @@ import java.awt.Rectangle;
 import java.awt.Cursor;
 import javax.swing.JLabel;
 import java.awt.Choice;
-import java.awt.List;
 import javax.swing.JComboBox;
 import java.awt.Font;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+
+import dao.impl.productDaoImpl;
+import model.product;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.util.List;
+import javax.swing.JScrollPane;
+import javax.swing.JTextPane;
+import javax.swing.ListSelectionModel;
+import java.awt.Component;
+import java.awt.Dimension;
 
 public class orderAddUI extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
+	private JTable table;
 
 	/**
 	 * Launch the application.
@@ -55,15 +68,46 @@ public class orderAddUI extends JFrame {
 		
 		JLabel lblNewLabel = new JLabel("");
 		lblNewLabel.setIcon(new ImageIcon("C:\\javaOCP\\javaPracticeShop\\javaPracticeHomework\\asset\\shoppingCartIconSpot.png"));
-		lblNewLabel.setBounds(538, 8, 21, 25);
+		lblNewLabel.setBounds(543, 19, 21, 25);
 		contentPane.add(lblNewLabel);
 		//set invisible at start
 		lblNewLabel.setVisible(false);
 		
 		
 		JPanel panel = new JPanel();
-		panel.setBounds(10, 144, 554, 170);
+		panel.setBounds(10, 81, 554, 252);
 		contentPane.add(panel);
+		panel.setLayout(null);
+		
+		table = new JTable();
+		table.setRowMargin(10);
+		table.setRowHeight(30);
+		table.setAutoResizeMode(JTable.AUTO_RESIZE_ALL_COLUMNS);
+		table.setRowSelectionAllowed(false);
+		table.setShowHorizontalLines(false);
+		table.setModel(new DefaultTableModel(
+			new Object[][] {
+				{"a", "s", "d"},
+				{"d", "d", "d"},
+				{"d", "d", null},
+			},
+			new String[] {
+				"name", "price", "New column"
+			}
+		) {
+			Class[] columnTypes = new Class[] {
+				String.class, String.class, Object.class
+			};
+			public Class getColumnClass(int columnIndex) {
+				return columnTypes[columnIndex];
+			}
+		});
+		table.getColumnModel().getColumn(0).setMinWidth(50);
+		table.getColumnModel().getColumn(1).setPreferredWidth(90);
+		table.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		table.setFont(new Font("新細明體", Font.PLAIN, 18));
+		table.setBounds(10, 66, 300, 128);
+		panel.add(table);
 		
 		JButton btnNewButton = new JButton("");
 		btnNewButton.addActionListener(new ActionListener() {
@@ -74,21 +118,34 @@ public class orderAddUI extends JFrame {
 		btnNewButton.setContentAreaFilled(false);
 		btnNewButton.setBorderPainted(false);
 		btnNewButton.setIcon(new ImageIcon("C:\\javaOCP\\javaPracticeShop\\javaPracticeHomework\\asset\\shoppingCartIcon.png"));
-		btnNewButton.setBounds(512, 10, 40, 32);
+		btnNewButton.setBounds(517, 21, 40, 32);
 		contentPane.add(btnNewButton);
 		
 		JComboBox comboBox = new JComboBox();
+		comboBox.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				
+			}
+		});
 		comboBox.setBorder(null);
 		comboBox.setFont(new Font("新細明體", Font.PLAIN, 16));
 		comboBox.setEditable(true);
-		comboBox.setBounds(33, 28, 159, 25);
+		comboBox.setBounds(33, 28, 219, 25);
 		contentPane.add(comboBox);
 		
-		comboBox.addItem("a");
-		comboBox.addItem("aa");
-		comboBox.addItem("aaa");
-		comboBox.addItem("aaaa");
+		productDaoImpl pd = new productDaoImpl();
+		List<product> l = pd.queryAllProduct();
+		
+		for(product p:l) {
+			System.out.println(p.getProduct_name());
+			comboBox.addItem(p.getProduct_name());
+		}
 		
 		comboBox.setSelectedItem("select service");
+		
+		JButton btnNewButton_1 = new JButton("New button");
+		btnNewButton_1.setBounds(477, 343, 87, 23);
+		contentPane.add(btnNewButton_1);
 	}
 }
